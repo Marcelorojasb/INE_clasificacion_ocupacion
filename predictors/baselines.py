341a7b6951672721a48148ec474763c551300c41
NicoCrocco <ncrocco@gmail.com>
2024-07-18 20:38:23 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   predictors/baselines.py

--- predictors/baselines.py
+++ predictors/baselines.py
@@ -1,9 +1,9 @@
+# El siguiente archivo realiza las predicciones en base a los modelos baseline
+
 import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-#import unidecodeall
-#import xgboost as xgb
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.pipeline import Pipeline, FeatureUnion
@@ -26,6 +26,24 @@ output_folder="data_out"
 output_path = os.path.join(os.getcwd(), output_folder)
 
 def predict_baseline(samples,carpeta,versions):
+    '''
+    ### predict_baseline(samples,  carpeta, version):
+    Crea la predicción de las clases en base a los modelos entrenados y entrega un csv con los textos y sus etiquetas predichas, \n
+    además de una lista con las predicciones que es utilizado para evaluar los modelos con la función validation().
+
+    ### Parámetros:
+    samples: pandas dataframe
+        Datos de entrenamiento
+
+    carpeta: str
+        Nombre de la carpeta en la que se quieren guardar las predicciones
+
+    version: str
+        Nombre del modelo a utilizar en la predicción
+
+    ### Resultados:
+    y_pred: predicciones del modelo
+    '''
     for version in versions:
         model = load('models/' + version + '.joblib') 
         predicted_probabilities = model.predict_proba(samples)
